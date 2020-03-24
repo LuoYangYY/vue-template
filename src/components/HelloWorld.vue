5dc635034d7d03288b5370dbec454edec104edf4
luoyang <luoyang@mogujie.com>
2020-03-24 14:07:15 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 class Msg {
-    public msg: string = 'hello world build';
+    public msg: string = 'hello world buildtest';
     GetMoreMsg(): string{
         return'webpack+vue+typescript+less'
     }
